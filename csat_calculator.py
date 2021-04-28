278cf4bea3b51455aeef8cfd1bf3e2cbc1a2971b
PanWu <ustcwupan@gmail.com>
2021-04-27 23:53:14 -0700

    Update csat_calculator.py

--- csat_calculator.py
+++ csat_calculator.py
@@ -12,11 +12,9 @@ import pandas as pd
 from scipy import stats
 
 st.title('CSAT Calculator')
-st.write('''Simplify your CSAT calculation with a statistical rigor.
-    Please input survey response result on the left-sidebar''')
-
-#############################################################################
-st.header('Part 1. CSAT calculation (without confidence interval)')
+st.write('**Straightforward CSAT calculation with statistical rigor.**')
+st.write('''Please input the survey response result on the left sidebar,
+    and the following table will be updated accordingly:''')
 # generate the side bar for survey result input, survey have 5 rating levels
 # default values are set with survey result {1, 5, 3, 4, 5, 2, 1, 4, 3, 4}
 default_response = [2, 1, 2, 3, 2]
@@ -26,12 +24,13 @@ res_list = [
         '# response with {0} star(s): '.format(i+1),
         min_value=0, max_value=None, step=1, value=default_response[i],
         key='res_list_{0}'.format(i), format='%d')
-    for i in range(n_level)
-]
+    for i in range(n_level)]
 survey_result = np.array(res_list)
 df_survey = pd.DataFrame({'rating': range(1, 6), 'count': survey_result})
-st.write(df_survey)
+st.dataframe(df_survey)
 
+#############################################################################
+st.header('Part 1. CSAT calculation (without confidence interval)')
 # calculate CSAT score and percentage without confidence interval calculation
 csat_score = (df_survey['rating'] * df_survey['count']).sum() \
     / df_survey['count'].sum()
@@ -43,22 +42,24 @@ st.write('... CSAT score is:         {:.2f}'.format(csat_score))
 st.write('... CSAT percentage is:    {:.2f}'.format(csat_percent))
 
 #############################################################################
-st.header('Part 2. CSAT (with confidence interval)')
+st.header('Part 2. CSAT (w/ confidence interval)')
 # generate the side bar for conf interval, default at 0.95
 conf_level = st.sidebar.slider(
     'Please choose the confidence level:',
     0.80, 0.99, value=0.95, step=0.01)
 survey_count = df_survey['count'].sum()
 degrees_freedom = survey_count - 1
-st.write('(degree of freedom, confidence level): ({0:d}, {1:.2f})'.format(
-    degrees_freedom, conf_level))
+st.write(
+    ''' Please adjust the confidence level on the left sidebar,
+    now the statistical test has the following parameters:''',
+    '(degree of freedom, confidence level) is ({0:d}, {1:.2f})'.format(
+        degrees_freedom, conf_level))
 
 df_survey_1 = df_survey.assign(diff=lambda x: x['rating'] - csat_score) \
         .assign(diff2=lambda x: x['diff'] ** 2 * x['count'])
 std_sample = np.sqrt(df_survey_1['diff2'].sum() / degrees_freedom)
 # calculate std for CSAT score
 std_csat_score = std_sample / np.sqrt(survey_count)
-
 # calculate std for CSAT percent
 std_csat_percent = np.sqrt(csat_percent * (1-csat_percent) / survey_count)
 
@@ -73,7 +74,7 @@ st.write('... CSAT percentage is: {0:.2f} ({1:.2f}, {2:.2f})'.format(
     csat_percent, csat_percent_low, csat_percent_high))
 
 #############################################################################
-st.header('Part 3. CSAT (with conf interval + finite population correction)')
+st.header('Part 3. CSAT (w/ confidence interval and finite population correction)')
 survey_total = st.number_input(
     '# Total survey sent out (regardless with response or not): ',
     value=survey_count, min_value=0, max_value=None, step=1,
